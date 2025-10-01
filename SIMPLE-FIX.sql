00693bd85cc6facd6a6d893cae29f887659daf3c
rthan99 <rthan99@users.noreply.github.com>
2025-10-01 11:13:56 +0200

    newest update

--- SIMPLE-FIX.sql
+++
@@ -1,8 +0,0 @@
--- SIMPLE ONE-COMMAND FIX FOR DUPLICATE VIDEOS
--- Copy this ENTIRE command and run it in Supabase SQL Editor:
-
-UPDATE projects
-SET video_urls = '{}'
-WHERE video_url IS NOT NULL 
-  AND video_url NOT LIKE '%/storage/v1/object/public/%';
-
